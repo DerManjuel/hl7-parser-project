9e3cdc65e9e0acd4207a5184834e9e1f53f5abf1
manuel.herbst@student.uni-luebeck.de <manuel.herbst@student.uni-luebeck.de>
2026-06-04 23:08:25 +0200

    added todos in test files

---
+++ tests/test_hl7_service.py
@@ -0,0 +1,9 @@
+# douplicate mesage
+# patient double
+# patient update
+# encounter double
+# encounter update
+# missing patient name
+# missing PV1 segment
+# missing patient name - detailed
+# processed message douplicate
